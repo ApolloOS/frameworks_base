0386f8dde426e5afec504b59bc5e316180d75b3c
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters
    Squashed:
    
        From: d34d <clark@cyngn.com>
        Date: Mon, 13 Feb 2017 01:12:57 -0500
        Subject: [PATCH 292/732] SystemUI: Show infinity for large notification counts
    
        This patch adds a new string to SystemUI for displaying the infinity
        symbol when a notification has a count larger than 999.  This only
        affects the notification icon in the status bar and does not change
        the text displayed in the actual notification.
    
        These changes were manually cherry-picked from cm-13.0.
    
        Change-Id: I3481aac13cd90ee20ed48194df3ec789c16c8b4b
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: jhonboy121 <alfredmathew05@gmail.com>
        Date: Sun, 2 Jan 2022 18:09:39 +0530
        Subject: SystemUI: Refactor notification counter
    
        * Update icons from NotificationIconAreaController when settings change and set initial state from StatusBarIconController
        * Also use value of config_statusBarShowNumber as the default
    
        [neobuddy89: Add required NPE checks to properly apply settings on boot.]
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Fran Palmero <sweetydesigns@gmail.com>
        Date: Thu, 21 Jul 2022 05:05:10 +0200
        Subject: SystemUI: Update notification count overlay
    
        * Updated to A12 style
        * Overlay picked from figma.com/community/plugin/1088610476491668236 edited with GIMP 2.10.18
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [neobuddy89: Updated and adapted for A14 QPR1]
    
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6915,6 +6915,12 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
+
+        /**
          * @hide
          */
         public static final String QS_TILE_LABEL_SIZE = "qs_tile_label_size";
