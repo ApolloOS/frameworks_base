ca930cb1ee12a2ab0010b075a0d667e0b2c5ac12
Luofan Chen <clfbbn@gmail.com>
2023-06-10 11:29:07 +0330

    base: Implement Lineage health service
    Change-Id: I772ccf6d323c24d681aa8468bf4318c7b73bd3f5
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -226,6 +226,7 @@ import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.apollo.LineageGlobalActionsService;
 import com.android.server.apollo.display.LiveDisplayService;
+import com.android.server.apollo.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 
@@ -2582,6 +2583,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartLiveDisplayService");
             mSystemServiceManager.startService(LiveDisplayService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartHealthService");
+            mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
         }
 
