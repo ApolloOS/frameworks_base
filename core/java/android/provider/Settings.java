9cdb833e74f66087b2d88fbc57b3ab05d72006d9
SamarV-121 <samar@samarv121.dev>
2023-06-18 12:28:14 +0530

    base: Add settings to change QS tile shape [1/2]
    Change-Id: If6ad2f69ac1d8ab15c20be9dbe8531a2ecc13aae
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12983,6 +12983,12 @@ public final class Settings {
         public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
 
         /**
+         * Quicksettings tile shape
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
