bc8a958120f0cf5e56c93cc7a64b8600b0c3b0ff
Pranav Vashi <neobuddy89@gmail.com>
2024-02-04 16:33:24 +0530

    Remove unused UDFPS_ANIM settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6739,11 +6739,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String UDFPS_ANIM = "udfps_anim";
-
-        /**
-         * @hide
-         */
         public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
 
         /**
