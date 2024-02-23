53b264ea04425cc92be461d60f5e0e3ca93af396
Dhina17 <dhinalogu@gmail.com>
2024-02-23 23:09:31 +0900

    SystemUI: Forward port 'HD & wifi calling statusbar icons'
    Squashed:
    * SystemUI: Add roaming indicator to statusbar tuner
    * SystemUI: Fix HD calling icon for RTL layout
    * SystemUI: sb: Fix VoWifi icon size
    * SystemUI: sb: Hide wifi airplane spacer when vowifi is available
    * SignalDrawable: Add support for Cut + Roaming back
    
    Adithya R <gh0strider.2k18.reborn@gmail.com>:
        HD icon shows for VoLTE/VoNR, and wifi calling for VoWiFi.
        Based on CLO's volte/vowifi icon implementation, reworked manually.
        Drawables taken from Nothing OS and manually tweaked.
    
    Dhina17 <dhinalogu@gmail.com>:
        Rewrite it for new status bar impl in Android 14.
        Move VoWifi icon to Wifi space.
        New VoWifi icon with SIM identification.
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Weijie Wang <quic_weijiew@quicinc.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: nift4 <nift4@protonmail.com>
    Original-Change-Id: I0190394690e9b38e0575ffb099be8a5ed2ae9d90
    Change-Id: Ib48b1295d060acf462b2f91e81cbb570fd8df42e
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6691,12 +6691,6 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
-         * Whether to display roaming indicator for roaming signal connection
-         * @hide
-         */
-        public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
-
-        /**
          * Whether to scramble a pin unlock layout
          * 0 = 0ff, 1 = on
          * @hide
